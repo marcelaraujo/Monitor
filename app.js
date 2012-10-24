7126c89524aac69acab790b3672b848ea95b4ddd
marcelaraujo <ceceldada@gmail.com>
2012-10-24 14:28:10 -0200

    Bug fix on package.json and ident app.js file.

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
-
 /**
  * Module dependencies.
  */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view options', { pretty: true });
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  //app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view options', {
+        pretty: true
+    });
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.cookieSession({
+        secret: 'asd98ua0sdu0as9ud0a8sd87h2378hd283h8ash9das',
+        cookie: {
+            maxAge: 60 * 60 * 1000
+        }
+    }));
+    app.use(express.methodOverride());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(app.router);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function () {
+    app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
 });
